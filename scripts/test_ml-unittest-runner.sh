2326c874223989c1bceeca645549850ef3d978a6
Chris Kelly <eurochriskelly@gmail.com>
2024-03-19 13:13:00 +0100

    Added changes to allow user to specify test after number.

--- scripts/test_ml-unittest-runner.sh
+++ scripts/test_ml-unittest-runner.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-#
 
 # Check that user has passed in the correct number of arguments
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <path-to-cup-repo>"
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 <path-to-cup-repo> [<after-test-number>]"
     exit 1
 fi
-
 dir=$1
+after_test_number=1
+if [ -n "$2" ]; then
+    after_test_number=$2
+fi
 if [ ! -d "$dir" ]; then
     echo "Error: $dir does not exist"
     exit 1
 fi
 
-export MLU_PORT_TEST=8043
-
-bash scripts/run_unit_tests.sh --directory $dir
+bash scripts/run_unit_tests.sh \
+  --directory $dir \
+  --test_port 8043 \
+  --after $after_test_number
